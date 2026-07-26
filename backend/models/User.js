15dbba2d41672658a4bbb869e891defe74b5a385
kallan <jojiag2005@gmail.com>
2026-07-26 22:16:09 +0530

    fixed some dependency issues

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,8 +4,12 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  email: {
+     type: String,
+      required: true,
+       unique: true, match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, lowercase: true, trim: true
+     },
+  password: { type: String, required: true, minlength: 6, maxlength: 20 },
   role: { type: String, default: 'user' } // This sets up your admin powers for later!
 }, { timestamps: true });
 
